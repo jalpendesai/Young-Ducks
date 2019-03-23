546359031ee5435464212fb2fa368feb078f8341
jalpendesai <djalpen24@gmail.com>
2019-03-23 08:03:59 -0400

    Added Basic Enemy Patrol

---
+++ Xaron/Assets/Scripts/Enemy/patrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+    public float speed;
+    public Transform groundDetaction;
+
+    private bool movingRight = true;
+
+    private void Update() {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetaction.position,Vector2.down,2f);
+
+        if(groundInfo.collider == false){
+            if(movingRight == true){
+                transform.eulerAngles = new Vector3(0,-180f,0);
+                movingRight = false;  
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0,0,0);
+                movingRight = true;
+            }
+        }
+    }
+}
---
+++ Xaron/Assets/Scripts/Platform_Boundary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Boundary : MonoBehaviour
+{
+    public float deadZone = -8f;
+
+    public Transform lastCheckPoint = null; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y < deadZone){
+            this.transform.position = lastCheckPoint.position;
+        }
+    }
+}
