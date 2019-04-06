f0c4b7aee540a1ed64165a02a595c4212136cc4c
jalpendesai <djalpen24@gmail.com>
2019-04-06 07:12:20 -0400

    Implemented Basic Game Menu

---
+++ Xaron/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        //  Loads next scene in a queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit(){
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
