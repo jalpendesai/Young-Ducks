b15d2f0872b20c07b7778e7f3d31482f86942f7a
jalpendesai <djalpen24@gmail.com>
2019-04-13 09:15:15 -0400

    Implemented Deadly Traps

---
+++ Xaron/Assets/Scripts/Environment/TrapCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrapCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            Debug.Log("Dead Meat");
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+        if(other.collider.CompareTag("Enemy")){
+            Destroy(other.gameObject);
+        }
+    }
+}
